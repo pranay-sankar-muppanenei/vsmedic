2bdc713ba0321968d1c1145d93d82876b1bb7767
pranay341 <ro200341@rguktong.ac.in>
2025-08-27 15:58:00 +0530

    Singla second update

--- frontend/src/layout/header.jsx
+++ frontend/src/layout/header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
         { name: "Services", path: "/services" },
         { name: "SrajanWomenWellness", path: "/srajan" },
         { name: "MultiSpeciality OPD's", path: "/opd" },
-        {name:"Singla Slimming Center",path:"/services/slimming"},
+        {name:"Singla Slimming Center",path:"/singla"},
       ],
     },
    
